35fa26cba91db2b36f0a28de7d3810379367e0e8
emcdunna <emcdunna@gmail.com>
2024-08-15 19:02:34 -0700

    make sure to allow sub typing

--- google_finance.py
+++ google_finance.py
@@ -1,10 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions
-from selenium.common.exceptions import NoAlertPresentException
-from enum import Enum
-import time
+from typing import List
 
 
 # Class to interact with the web application for finance info
